f3232ca01553ea8956607a6dc0b4df1863658b08
CengNing <siqulie@iCloud.com>
2024-02-07 13:29:24 +0800

    创建SSE端点 实现事件流逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use futures::stream::StreamExt;
+use tokio::time::{self, Duration};
+use warp::sse::Event;
+use warp::Filter;
+
+
+async fn sse_events() -> impl warp::Reply {
+    // 实现事件流逻辑
+    let mut interval = time::interval(Duration::from_secs(1));
+    futures::stream::unfold((), move |_| async move {
+        interval.tick().await;
+        Some((Event::default().data("hello"), ()))
+    })
+}
+
+#[tokio::main]
+async fn main() {
+    let sse_route = warp::path("events")
+        .and(warp::get())
+        .map(|| {
+            let stream = sse_events();   //调用异步函数获取事件流
+            warp::sse::reply(warp::sse::keep_alive().stream(stream))
+        });
+    warp::serve(sse_route).run(((127, 0, 0, 1), 3030)).await;
 }
